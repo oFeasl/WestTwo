708af5ac0c34c76b15353a8a25bea1d105daa6a1
oFeasl <68706543+oFeasl@users.noreply.github.com>
2021-03-18 17:19:44 +0800

    Update flask_API.py

--- FlaskAPI/flask_API.py
+++ FlaskAPI/flask_API.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,redirect,url_for
 from flask import json
 from flask.json import jsonify
 from werkzeug.exceptions import abort
@@ -20,20 +20,55 @@ tasks = [
     }
 ]
 
+errors = [
+    {
+        "code":404,
+        "state":"Page Not Found!"
+    },
+    {
+        "code":401,
+        "state":"401"
+    },
+    {
+        "code":402,
+        "state":"402"
+    },
+]
+
+status = [
+    {
+        "code":1,
+        "state":"Delete Successfully!"
+    },
+    {
+        "code":2,
+        "state":"Id No Found!"
+    }
+]
+
 @app.route("/")
 def index():
     abort(404)
-    return jsonify(tasks)
+    
 
 @app.route("/display_tasks")
 def display_tasks():
     return jsonify(tasks)
 
 
+@app.route("/del_task/<int:id>")
+def del_task(id):
+    for i in tasks :
+        if i["id"]==id:
+            tasks.remove(i)
+            return redirect(url_for("display_tasks"))
+    return jsonify(status[1])
+
+
 
 @app.errorhandler(404)
 def nofound(error):
-    return "NoFound"
+    return jsonify(errors[0])
 
 if __name__=="__main__":
     app.run(debug=1)
