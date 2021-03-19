c4c55de09c24b3722d98f08c9671de9c2a5d51c7
oFeasl <68706543+oFeasl@users.noreply.github.com>
2021-03-19 08:46:20 +0800

    Update flask_API.py

--- FlaskAPI/flask_API.py
+++ FlaskAPI/flask_API.py
@@ -81,6 +81,10 @@ def display_tasks():
     return jsonify(tasks)
 
 
+
+
+#####################删除###########################
+
 # 删除接口，返回data中带有删除的task
 @app.route("/del_task/<int:id>",methods=["DELETE"])
 def del_task(id):
@@ -88,8 +92,42 @@ def del_task(id):
         if i["id"]==id:
             del_task = i
             tasks.remove(i)
-            return jsonify({"status":0,"message":"删除成功","data":del_task})
+            return jsonify(return_Feedback(status=0,message="删除成功",data=del_task))
     return jsonify(status[1])
+
+
+# 删除所有已完成事项
+@app.route("/del_all_done",methods=["DELETE"])
+def del_all_done():
+    temp_task1 = []
+    for i in tasks:
+        if i["done"] == True:
+            temp_task1.append(i)
+    for j in temp_task1:
+        tasks.remove(j)
+    return jsonify(return_Feedback(status=0,message="删除成功",data=temp_task1))
+
+
+# 删除所有已代办事项
+@app.route("/del_all_undone",methods=["DELETE"])
+def del_all_undone():
+    temp_task2 = []
+    for i in tasks:
+        if i["done"] == False:
+            temp_task2.append(i)
+    for j in temp_task2:
+        tasks.remove(j)
+    return jsonify(return_Feedback(status=0,message="删除成功",data=temp_task2))
+
+
+#######################删除#########################
+
+
+
+
+
+
+#######################新增#########################
 
 
 # 新增task接口，返回data中带有新增的task
@@ -107,7 +145,16 @@ def add_task():
     return jsonify(return_Feedback(status=0,message="",data=task))
 
 
+#######################新增#########################
 
+
+
+
+
+
+
+
+#######################更改#########################
 
 # 将1条task设为已办
 @app.route("/set_a_done/<int:id>",methods=["PUT"])
@@ -127,16 +174,12 @@ def set_a_undone_(id):
             return jsonify(return_Feedback(status=0,message="",data=i))
     return jsonify(return_Feedback(status=1,message="Id Not Found",data={}))
 
-
-
 # 将所有task设为已办
 @app.route("/set_all_done",methods=["PUT"])
 def set_all_done_():
     for i in tasks:
         i["done"]=True
     return jsonify(return_Feedback(status=0,message="All Tasks Are Done",data={}))
-
-
 
 # 将所有task设为待办
 @app.route("/set_all_undone",methods=["PUT"])
@@ -145,8 +188,20 @@ def set_all_undone_():
         i["done"]=False
     return jsonify(return_Feedback(status=0,message="All Tasks Are Undone",data={}))
 
+#######################更改#########################
 
 
+
+
+
+#######################查找#########################
+
+
+
+
+
+
+#######################查找#########################
 
 
 
