2b9c68305a1ac594386e57ce930b55527c8028dc
oFeasl <68706543+oFeasl@users.noreply.github.com>
2021-04-01 20:54:06 +0800

    n

--- FlaskAPI/test_time.py
+++
@@ -1,8 +0,0 @@
-import time
-
-year = (time.localtime(time.time()).tm_year)
-month = (time.localtime(time.time()).tm_mon)
-day = (time.localtime(time.time()).tm_mday)
-
-current_time = (str)(year)+"."+(str)(month)+"."+(str)(day)
-print(current_time)
